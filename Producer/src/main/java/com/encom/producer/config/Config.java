ae7fa0636bd460410140175bbf22f16df980fa4a
Elkhan <is.elxan@gmail.com>
2021-12-15 01:55:04 +0400

    feature: add producer & consumer basic logic

---
+++ Producer/src/main/java/com/encom/producer/config/Config.java
@@ -0,0 +1,15 @@
+package com.encom.producer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.support.converter.ByteArrayJsonMessageConverter;
+
+@EnableKafka
+@Configuration
+public class Config {
+    @Bean
+    public ByteArrayJsonMessageConverter jsonConverter() {
+        return new ByteArrayJsonMessageConverter();
+    }
+}
---
+++ Producer/src/main/java/com/encom/producer/model/DataDto.java
@@ -0,0 +1,15 @@
+package com.encom.producer.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class DataDto {
+    private String title;
+    private String description;
+}
